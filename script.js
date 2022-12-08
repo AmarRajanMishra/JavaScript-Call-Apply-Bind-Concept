d8e7a5f89a590a326a9842545721dda19452ee9f
Amar <amar1998mishra@gmail.com>
2022-12-08 21:25:25 +0530

    JavaScript => call(), apply(), bind() method

--- script.js
+++ script.js
@@ -1,42 +1,89 @@
-// this is keyword which tell it from where
+// // this is keyword which tell it from where
 
-let data1 = {
-    Name:"Kamlesh",
-    Age: 22,
-    Gender:"Male",
+// let data1 = {
+//     Name:"Kamlesh",
+//     Age: 22,
+//     Gender:"Male",
     
-}
-let myDetails=function(city, state){
-    console.log(this.Name + " " + city + " " + state)
+// }
+// let myDetails=function(city, state){
+//     console.log(this.Name + " " + city + " " + state)
 
-}
+// }
 
-let data2 = {
-    Name:"P. L.",
-    Age: 28,
-    Gender:"Male",
+// let data2 = {
+//     Name:"P. L.",
+//     Age: 28,
+//     Gender:"Male",
     
+// }
+// // Call, Apply, Bind
+
+//     /* Call Method */
+// // Function Borrow
+// myDetails.call(data1, 'Ayodhya', 'U.P.')
+// myDetails.call(data2, 'Ayodhya', 'U.P.')
+
+//     /* Apply Method */
+
+// myDetails.apply(data1, ['Ayodhya', 'U.P.'])
+// myDetails.apply(data2, ['Ayodhya', 'U.P.'])
+
+
+//     /* Bind Method */
+
+// let details1 = myDetails.bind(data1, ['Ayodhya', 'U.P.'])
+// let details2 = myDetails.bind(data2, ['Ayodhya', 'U.P.'])
+// // As function treat kr rhe hai
+
+// //    console.log(details1)
+//    console.log(details1())
+// //    console.log(details2)
+//    console.log(details2())
+
+
+
+// JavaScript Closure Concept
+
+const sum = (a, d)=>{
+    console.log("The value of A is "+a + "and b is " +d)
+    const c = 10; // Lexical Scope
+    return function(b){ //Anonymous
+        return a+b+c+d;
+
+
+    }
+
+
 }
-// Call, Apply, Bind
+// a = 5, b = 20, c = 30
+// sum(5)  // Calling
+var storage = sum(5, 15) // Store whole sum function inside storage
+console.log(storage(20))
 
-    /* Call Method */
-// Function Borrow
-myDetails.call(data1, 'Ayodhya', 'U.P.')
-myDetails.call(data2, 'Ayodhya', 'U.P.')
+// function addNum(parameter){
 
-    /* Apply Method */
+// }
 
-myDetails.apply(data1, ['Ayodhya', 'U.P.'])
-myDetails.apply(data2, ['Ayodhya', 'U.P.'])
+// const addNum2 = (parameter) =>{
+
+// }
+//  var addNum3 = function(parameter){
+
+//  }
 
 
-    /* Bind Method */
+//  const sumTwo = (a, d)=>{
+//     console.log("The value of A is "+a + "and b is " +d)
+//     const c = 10; // Lexical Scope
+//     return function innerFun(b){ 
+//         return a+b+c+d;
+//     }
+// }
+// var storage = sumTwo(5, 15) // Store whole sum function inside storage
+// console.log(storage)
+// console.log(storage(20))
 
-let details1 = myDetails.bind(data1, ['Ayodhya', 'U.P.'])
-let details2 = myDetails.bind(data2, ['Ayodhya', 'U.P.'])
-
-   console.log(details1)
-   console.log(details2)
 
 
 
